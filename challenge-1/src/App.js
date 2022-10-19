50a3cda955ba997557eef251554b19f45859c67e
Abhishek Singh <abhisheksingh16102000@bbdu.ac.in>
2022-10-20 01:40:11 +0530

    challenge-1 completed basic of react rendering some component

---
+++ challenge-1/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <>
+     <h1>Abhishek Singh</h1>
+     <p>
+     Wikipedia is hosted by the Wikimedia Foundation, a non-profit organization that also hosts a range of other projects.
+     </p>
+     <h3>Brother's Name</h3>
+     <ul>
+      <li>Sudhir</li>
+      <li>Rahul</li>
+      <li>Krishna</li>
+      <li>Ramakant</li>
+      <li>Prince</li>
+      <li>Rohit</li>
+      <li>Raju</li>
+      <li>Atul</li>
+      <li>Lucky</li>
+      <li>Abhay</li>
+     </ul>
+   </>
+  );
+}
+
+export default App;
